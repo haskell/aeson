1bc5cf9c1ab37b997cb2c943c70339a2e9c65cbb
Bryan O'Sullivan <bos@serpentine.com>
2011-02-11 20:20:57 -0800

    Resimplify the parsing of escaped text

--- Data/Aeson/Parser.hs
+++ Data/Aeson/Parser.hs
@@ -102,8 +102,8 @@ reparse p s = case parse p s `feed` "" of
                 _            -> fail "unexpected failure"
 
 unescape :: Parser ByteString
-unescape = toByteString <$> go (Right mempty) where
-  go (Right acc) = do
+unescape = toByteString <$> go mempty where
+  go acc = do
     h <- A.takeWhile (/=backslash)
     let rest = do
           start <- A.take 2
@@ -113,9 +113,9 @@ unescape = toByteString <$> go (Right mempty) where
                          Just i -> i
                          _      -> 255
           if slash /= backslash || escape == 255
-            then go (Left "invalid JSON escape sequence")
-            else flip (<|>) (go (Left "invalid Unicode escape sequence")) $ do
-            let cont m = go (Right (acc `mappend` fromByteString h `mappend` m))
+            then fail "invalid JSON escape sequence"
+            else do
+            let cont m = go (acc `mappend` fromByteString h `mappend` m)
                 {-# INLINE cont #-}
             if t /= 117 -- 'u'
               then cont (fromWord8 (B.unsafeIndex mapping escape))
@@ -130,8 +130,10 @@ unescape = toByteString <$> go (Right mempty) where
                                        (b - 0xdc00) + 0x10000
                               in cont (fromChar (chr c))
                          else fail "invalid UTF-16 surrogates"
-    rest <|> return (acc `mappend` fromByteString h)
-  go (Left err) = fail err
+    done <- atEnd
+    if done
+      then return (acc `mappend` fromByteString h)
+      else rest
   mapping = "\"\\/\n\t\b\r\f"
 
 hexQuad :: Parser Int
