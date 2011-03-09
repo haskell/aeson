76bab1cd5e18b8daf5437539899fc98cabf5563f
Bryan O'Sullivan <bos@mailrank.com>
2011-03-09 14:04:49 -0800

    Simplify and extend map support.

--- Data/Aeson/Functions.hs
+++ Data/Aeson/Functions.hs
@@ -3,10 +3,18 @@ module Data.Aeson.Functions
       hashMap
     , mapHash
     , transformMap
+    -- * String conversions
+    , decode
+    , strict
+    , lazy
     ) where
 
 import Control.Arrow ((***), first)
 import Data.Hashable (Hashable)
+import Data.Text (Text)
+import Data.Text.Encoding (decodeUtf8, encodeUtf8)
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Lazy as L
 import qualified Data.HashMap.Strict as H
 import qualified Data.Map as M
 
@@ -27,3 +35,15 @@ hashMap fk kv = M.fromList . map (fk *** kv) . H.toList
 mapHash :: (Eq k2, Hashable k2) => (k1 -> k2) -> M.Map k1 v -> H.HashMap k2 v
 mapHash fk = H.fromList . map (first fk) . M.toList
 {-# INLINE mapHash #-}
+
+strict :: L.ByteString -> Text
+strict = decode . B.concat . L.toChunks
+{-# INLINE strict #-}
+
+lazy :: Text -> L.ByteString
+lazy = L.fromChunks . (:[]) . encodeUtf8
+{-# INLINE lazy #-}
+
+decode :: B.ByteString -> Text
+decode = decodeUtf8
+{-# INLINE decode #-}
