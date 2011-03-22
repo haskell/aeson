9213f47e4870f426db108002bc703fd7ac63d685
Bryan O'Sullivan <bos@mailrank.com>
2011-03-22 10:13:16 -0700

    Add some basic round-trip tests.

--- tests/Properties.hs
+++ tests/Properties.hs
@@ -1,4 +1,7 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 import Data.Aeson.Encode
+import Data.Aeson.Parser (value)
 import Data.Aeson.Types
 import Data.Attoparsec.Number
 import Test.Framework (defaultMain, testGroup)
@@ -6,9 +9,17 @@ import Test.Framework.Providers.QuickCheck2 (testProperty)
 import Test.QuickCheck (Arbitrary(..))
 import Test.QuickCheck.Monadic (assert, monadicIO, run)
 import qualified Data.ByteString.Lazy.Char8 as L
+import qualified Data.Attoparsec.Lazy as L
 
 encodeDouble d  = encode (Number (D d)) == L.pack (show d)
 encodeInteger i = encode (Number (I i)) == L.pack (show i)
+
+roundTrip :: (Eq a, FromJSON a, ToJSON a) => a -> Bool
+roundTrip i = (fmap fromJSON . L.maybeResult . L.parse value . encode . toJSON) i == Just (Success i)
+
+roundTripBool (v::Bool) = roundTrip v
+roundTripDouble (v::Double) = roundTrip v
+roundTripInteger (v::Integer) = roundTrip v
 
 main = defaultMain tests
 
@@ -16,5 +27,10 @@ tests = [
   testGroup "encode" [
       testProperty "encodeDouble" encodeDouble
     , testProperty "encodeInteger" encodeInteger
+    ],
+  testGroup "roundTrip" [
+      testProperty "roundTripBool" roundTripBool
+    , testProperty "roundTripDouble" roundTripDouble
+    , testProperty "roundTripInteger" roundTripInteger
     ]
   ]
