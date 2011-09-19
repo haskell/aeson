0e03c0965ceee31060c8f880fa19d40e185c50ec
Bryan O'Sullivan <bos@serpentine.com>
2011-09-19 18:20:20 -0500

    Add the oft-requested decode function.

--- Data/Aeson.hs
+++ Data/Aeson.hs
@@ -28,11 +28,24 @@ module Data.Aeson
     , (.:)
     , (.:?)
     , object
-    -- * Encoding and parsing
+    -- * Encoding and decoding
+    , decode
     , encode
+    -- * Parsing
     , json
     ) where
 
 import Data.Aeson.Encode
 import Data.Aeson.Parser
 import Data.Aeson.Types
+import qualified Data.ByteString.Lazy as L
+import qualified Data.Attoparsec.Lazy as L
+
+-- | Efficiently deserialize a JSON value from a lazy 'L.ByteString'.
+decode :: (FromJSON a) => L.ByteString -> Maybe a
+decode s = case L.parse json s of
+             L.Done _ v -> case fromJSON v of
+                             Success a -> Just a
+                             _         -> Nothing
+             _          -> Nothing
+{-# INLINE decode #-}
