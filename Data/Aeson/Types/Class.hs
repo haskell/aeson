d2e9655bff28ed0848ce3841cf3dec26e3e95969
Luis Cabellos <zhen.sydow@gmail.com>
2011-11-16 13:55:35 +0100

    add new accessor (.:/) to allow default value

--- Data/Aeson/Types/Class.hs
+++ Data/Aeson/Types/Class.hs
@@ -33,6 +33,7 @@ module Data.Aeson.Types.Class
     , fromJSON
     , (.:)
     , (.:?)
+    , (.:/)
     , (.=)
     , typeMismatch
     ) where
@@ -706,6 +707,20 @@ obj .:? key = case M.lookup key obj of
                Nothing -> pure Nothing
                Just v  -> parseJSON v
 {-# INLINE (.:?) #-}
+
+-- | Retrieve the value associated with the given key of an 'Object'.
+-- The result is a default value if the key is not present, or 'empty' 
+-- if the value cannot be converted to the desired type.
+--
+-- This accessor is most useful if the key and value can be absent 
+-- from an object without affecting its validity and we know a 
+-- default value to assign in that case.  If the key and value 
+-- are mandatory, use '(.:)' instead.
+(.:/) :: (FromJSON a) => Object -> (Text, a) -> Parser a
+obj .:/ (key, val) = case M.lookup key obj of
+               Nothing -> pure val
+               Just v  -> parseJSON v
+{-# INLINE (.:/) #-}
 
 
 -- | Fail parsing due to a type mismatch, with a descriptive message.
