48546d933f4996c41a17a00dddc3f6ce1ba688f6
Bryan O'Sullivan <bos@serpentine.com>
2013-01-02 09:19:25 -0800

    Fix build on GHC 6.12

--- Data/Aeson/TH.hs
+++ Data/Aeson/TH.hs
@@ -96,7 +96,7 @@ import Prelude             ( String, (-), Integer, fromIntegral, error )
 import Text.Printf         ( printf )
 import Text.Show           ( show )
 #if __GLASGOW_HASKELL__ < 700
-import Control.Monad       ( (>>=) )
+import Control.Monad       ( (>>=), (>>) )
 import Prelude             ( fromInteger )
 #endif
 -- from unordered-containers:
