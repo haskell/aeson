7e3b4b38bd190171589faa97627cdd4b1157f01d
Bas van Dijk <v.dijk.bas@gmail.com>
2013-04-23 05:19:05 +0200

    First stab at a selector EDSL

---
+++ Data/Aeson/Select.hs
@@ -0,0 +1,132 @@
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
+-- (C) 2013 Bas van Dijk <v.dijk.bas@gmail.dom>
+module Data.Aeson.Select
+    ( -- * Selectors
+      (:->)
+    , runSelect
+    , select
+
+     -- * Value selectors
+    , obj, array, text, number, bool
+
+     -- * Object selectors
+    , field
+    , path
+
+      -- * Number selectors
+    , integer, double
+    ) where
+
+
+--------------------------------------------------------------------------------
+-- Imports
+--------------------------------------------------------------------------------
+
+import Control.Applicative
+import Control.Arrow
+import Control.Category
+import Control.Monad
+import Data.Aeson.Types
+import Data.Attoparsec.Char8 (Number(..))
+import Data.Text (Text)
+import Prelude hiding ((.))
+
+
+--------------------------------------------------------------------------------
+-- Select
+--------------------------------------------------------------------------------
+
+-- | Select @b@ from @a@.
+newtype a :-> b = Select {unSelect :: Kleisli Parser a b}
+    deriving ( Category
+             , Arrow
+             , ArrowZero
+             , ArrowPlus
+             , ArrowChoice
+             , ArrowApply
+             )
+
+runSelect :: (a :-> b) -> (a -> Parser b)
+runSelect = runKleisli . unSelect
+{-# INLINE runSelect #-}
+
+select :: (a -> Parser b) -> (a :-> b)
+select = Select . Kleisli
+{-# INLINE select #-}
+
+
+--------------------------------------------------------------------------------
+-- Value Selectors
+--------------------------------------------------------------------------------
+
+-- | Select an 'Object' from a 'Value'.
+obj :: Value :-> Object
+obj = select $ withObject "Object" pure
+{-# INLINE obj #-}
+
+-- | Select an 'Array' from a 'Value'.
+array :: Value :-> Array
+array = select $ withArray "Array" pure
+{-# INLINE array #-}
+
+-- | Select a 'Text' from a 'Value'.
+text :: Value :-> Text
+text = select $ withText "Text" pure
+{-# INLINE text #-}
+
+-- | Select a 'Number' from a 'Value'.
+number :: Value :-> Number
+number = select $ withNumber "Number" pure
+{-# INLINE number #-}
+
+-- | Select a 'Bool' from a 'Value'.
+bool :: Value :-> Bool
+bool = select $ withBool "Bool" pure
+{-# INLINE bool #-}
+
+
+--------------------------------------------------------------------------------
+-- Object Selectors
+--------------------------------------------------------------------------------
+
+-- | Select the named field from the 'Object'.
+field :: Text -> Object :-> Value
+field label = select (.: label)
+{-# INLINE field #-}
+
+-- | Select a path from nested @'Object's@.
+path :: [Text] -> Object :-> Value
+path [] = select $ \_ -> fail "attempted to traverse an Object without a path"
+path [key]      = field key
+path (key:keys) = field key >>> obj >>> path keys
+{-# INLINE path #-}
+
+
+--------------------------------------------------------------------------------
+-- Number Selectors
+--------------------------------------------------------------------------------
+
+-- | Select an 'Integer' from a 'Number'.
+integer :: Number :-> Integer
+integer = select $ \num -> case num of
+                             I i -> pure i
+                             D _ -> mismatch "Integer" "Double"
+{-# INLINE integer #-}
+
+-- | Select a 'Double' from a 'Number'.
+double :: Number :-> Double
+double = select $ \num -> case num of
+                             D d -> pure d
+                             I _ -> mismatch "Double" "Integer"
+{-# INLINE double #-}
+
+
+--------------------------------------------------------------------------------
+-- Failures
+--------------------------------------------------------------------------------
+
+mismatch :: String -> String -> Parser a
+mismatch expected name = fail $ "when expecting a " ++ expected ++
+                                ", encountered " ++ name ++ " instead"
