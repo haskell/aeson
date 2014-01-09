c19f6064dcfebdc5677b877ae98363683e1289df
Bryan O'Sullivan <bos@serpentine.com>
2014-01-08 22:04:18 -0800

    Rework Python encode/decode benchmarks to work with harness

--- benchmarks/encode.py
+++ benchmarks/encode.py
@@ -2,6 +2,16 @@
 
 import json, sys, time
 
+def isint(x):
+    try:
+        int(x)
+        return True
+    except:
+        return False
+
+if len(sys.argv) > 2 and isint(sys.argv[1]) and isint(sys.argv[2]):
+    sys.argv.pop(1)
+
 count = int(sys.argv[1])
 
 for n in sys.argv[2:]:
@@ -11,4 +21,4 @@ for n in sys.argv[2:]:
     for i in xrange(count):
         json.dumps(obj)
     end = time.time()
-    print ' ', end - start
+    print '  %d good, %gs' % (count, end - start)
