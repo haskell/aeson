c0afa596e5f6ac8b95a616efdb6215ebf8f93bd9
Bas van Dijk <v.dijk.bas@gmail.com>
2014-05-12 10:35:47 +0200

    Use scientific >= 0.3.1
    scientific-0.3.1 exports the Text and ByteString Scientific Builders from
    the following modules respectively:
    
    - Data.Text.Lazy.Builder.Scientific
    - Data.ByteString.Builder.Scientific

--- Data/Aeson/Encode/ByteString.hs
+++ Data/Aeson/Encode/ByteString.hs
@@ -19,11 +19,12 @@ module Data.Aeson.Encode.ByteString
 import Prelude hiding (null)
 import Data.Aeson.Types (ToJSON(..), Value(..))
 import Data.Char (ord)
-import Data.Scientific (Scientific, coefficient, base10Exponent, formatScientific, FPFormat(Generic))
+import Data.Scientific (Scientific, coefficient, base10Exponent)
 import Data.Word (Word8)
 import Data.Monoid (mappend)
 import           Data.ByteString.Builder      as B
 import           Data.ByteString.Builder.Prim as BP
+import           Data.ByteString.Builder.Scientific (scientificBuilder)
 import qualified Data.ByteString.Lazy as L
 import qualified Data.HashMap.Strict as HMS
 import qualified Data.Text as T
@@ -95,7 +96,7 @@ string t =
 
 number :: Scientific -> Builder
 number s
-    | e < 0     = B.string8 $ formatScientific Generic Nothing s
+    | e < 0     = scientificBuilder s
     | otherwise = B.integerDec (coefficient s * 10 ^ e)
   where
     e = base10Exponent s
