596b8084275b5e6f8163a2ed8c6926e0d78fc21a
Bryan O'Sullivan <bos@serpentine.com>
2015-05-26 08:48:15 -0700

    Finally fix attoparsec deprecation warnings

--- benchmarks/AesonParse.hs
+++ benchmarks/AesonParse.hs
@@ -3,7 +3,7 @@
 import Control.Exception
 import Control.Monad
 import Data.Aeson
-import Data.Attoparsec (IResult(..), parseWith)
+import Data.Attoparsec.ByteString (IResult(..), parseWith)
 import Data.Time.Clock
 import System.Environment (getArgs)
 import System.IO
