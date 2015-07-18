2980e4e131e86e32ae3a28db7051babcde47e555
Bryan O'Sullivan <bos@serpentine.com>
2015-07-17 20:15:15 -0700

    Clean up date benchmark

--- benchmarks/Dates.hs
+++ benchmarks/Dates.hs
@@ -1,22 +1,16 @@
-
-import Control.Exception
-import Control.Monad
-import Data.Aeson
+import Criterion.Main
+import Data.Aeson (decode)
 import Data.Time.Clock (UTCTime)
 import qualified Data.ByteString.Lazy as BL
-import qualified Data.ByteString as B
-import Criterion.Main
 
-utcTime :: BL.ByteString -> [UTCTime]
-utcTime str = let Just t = decode str in t
+utcTime :: BL.ByteString -> Maybe [UTCTime]
+utcTime = decode
 
 main :: IO ()
 main = do
   let file1 = "json-data/dates.json"
   let file2 = "json-data/dates-fract.json"
-  enA1 <- BL.readFile file1
-  enA2 <- BL.readFile file2
-  defaultMain
-    [ bench "utc-time" $ nf utcTime enA1
-    , bench "utc-time fract" $ nf utcTime enA2
+  defaultMain [
+      env (BL.readFile file1) $ \ ~bs -> bench "whole" $ nf utcTime bs
+    , env (BL.readFile file2) $ \ ~bs -> bench "fractional" $ nf utcTime bs
     ]
