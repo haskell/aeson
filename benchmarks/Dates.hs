9cf51f9fb6bf5b7f612e82cf22e31ccf341d882e
Bryan O'Sullivan <bos@serpentine.com>
2015-07-17 22:28:40 -0700

    Add ZonedTime benchmarks

--- benchmarks/Dates.hs
+++ benchmarks/Dates.hs
@@ -1,16 +1,26 @@
 import Criterion.Main
 import Data.Aeson (decode)
 import Data.Time.Clock (UTCTime)
+import Data.Time.LocalTime (ZonedTime)
 import qualified Data.ByteString.Lazy as BL
 
 utcTime :: BL.ByteString -> Maybe [UTCTime]
 utcTime = decode
+
+zTime :: BL.ByteString -> Maybe [ZonedTime]
+zTime = decode
 
 main :: IO ()
 main = do
   let file1 = "json-data/dates.json"
   let file2 = "json-data/dates-fract.json"
   defaultMain [
-      env (BL.readFile file1) $ \ ~bs -> bench "whole" $ nf utcTime bs
-    , env (BL.readFile file2) $ \ ~bs -> bench "fractional" $ nf utcTime bs
+      bgroup "UTCTime" [
+        env (BL.readFile file1) $ \ ~bs -> bench "whole" $ nf utcTime bs
+      , env (BL.readFile file2) $ \ ~bs -> bench "fractional" $ nf utcTime bs
+      ]
+    , bgroup "ZonedTime" [
+        env (BL.readFile file1) $ \ ~bs -> bench "whole" $ nf zTime bs
+      , env (BL.readFile file2) $ \ ~bs -> bench "fractional" $ nf zTime bs
+      ]
     ]
