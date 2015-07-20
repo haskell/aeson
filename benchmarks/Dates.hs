b3cb3b07dd2ee30af5f2ffd9f5c31322a9e3a141
Bryan O'Sullivan <bos@serpentine.com>
2015-07-20 10:07:00 -0700

    Add date/time encoding benchmarks

--- benchmarks/Dates.hs
+++ benchmarks/Dates.hs
@@ -1,5 +1,5 @@
 import Criterion.Main
-import Data.Aeson (decode)
+import Data.Aeson (decode, encode)
 import Data.Time.Clock (UTCTime)
 import Data.Time.LocalTime (ZonedTime)
 import qualified Data.ByteString.Lazy as BL
@@ -12,15 +12,27 @@ zTime = decode
 
 main :: IO ()
 main = do
-  let file1 = "json-data/dates.json"
-  let file2 = "json-data/dates-fract.json"
+  let file1 = BL.readFile "json-data/dates.json"
+  let file2 = BL.readFile "json-data/dates-fract.json"
   defaultMain [
-      bgroup "UTCTime" [
-        env (BL.readFile file1) $ \ ~bs -> bench "whole" $ nf utcTime bs
-      , env (BL.readFile file2) $ \ ~bs -> bench "fractional" $ nf utcTime bs
+      bgroup "decode" [
+        bgroup "UTCTime" [
+          env file1 $ \ ~bs -> bench "whole" $ nf utcTime bs
+        , env file2 $ \ ~bs -> bench "fractional" $ nf utcTime bs
+        ]
+      , bgroup "ZonedTime" [
+          env file1 $ \ ~bs -> bench "whole" $ nf zTime bs
+        , env file2 $ \ ~bs -> bench "fractional" $ nf zTime bs
+        ]
       ]
-    , bgroup "ZonedTime" [
-        env (BL.readFile file1) $ \ ~bs -> bench "whole" $ nf zTime bs
-      , env (BL.readFile file2) $ \ ~bs -> bench "fractional" $ nf zTime bs
+    , bgroup "encode" [
+        bgroup "UTCTime" [
+          env (utcTime <$> file1) $ \ ~ts -> bench "whole" $ nf encode ts
+        , env (utcTime <$> file2) $ \ ~ts -> bench "fractional" $ nf encode ts
+        ]
+      , bgroup "ZonedTime" [
+          env (zTime <$> file1) $ \ ~ts -> bench "whole" $ nf encode ts
+        , env (zTime <$> file2) $ \ ~ts -> bench "fractional" $ nf encode ts
+        ]
       ]
     ]
