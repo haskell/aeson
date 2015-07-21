6b8acde7095fe113b85c45255d5fa538d5741668
Leon P Smith <leon@melding-monads.com>
2015-07-21 02:05:28 -0400

    Fix utc offset parsing
    As I understand ISO 8601:2004,  ±HHMM is not a valid syntax for specifying
    a UTC offset in the context we are using it.   Only ±HH and ±HH:MM are
    valid in the extended ISO 8601 formats.
    
    I probably would weakly prefer to not accept ±HHMM, however,  because the
    the ToJSON ZonedTime instance has been emitting this syntax since aeson
    version 0.6.1.0 released on 2012-12-14,   our hands may well be tied on
    this matter.
    
    I can only hope that the older FromJSON instances will also accept ±HH and
    ±HH:MM,  so that we can fix the emitted syntax with minimal disruption,
    but knowing the time library, this probably is not the case.

--- tests/UnitTests.hs
+++ tests/UnitTests.hs
@@ -102,8 +102,8 @@ utcTimeGood = do
   assertEqual "utctime" (parseWithRead "%FT%T%QZ" ts2) t2
   assertEqual "utctime" (parseWithRead "%F %T%QZ" ts3) t3
   assertEqual "utctime" (parseWithRead "%F %T%QZ" ts4) t4
-  -- timezones.  Both +HHMM and +HH:MM are allowed for timezone offset
-  let ts5 = "2015-01-01T12:30:00.00+0000" :: LT.Text
+  -- offsets.  ±HH, ±HHMM, and ±HH:MM are allowed for utc offset
+  let ts5 = "2015-01-01T12:30:00.00+00" :: LT.Text
   let ts6 = "2015-01-01T12:30:00.00+01:15" :: LT.Text
   let ts7 = "2015-01-01T12:30:00.00-0200" :: LT.Text
   let ts8 = "2015-01-01T22:00:00.00-03:00" :: LT.Text
