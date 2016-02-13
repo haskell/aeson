9bef1ec066d98c46e7e5218ae0d48bdbc9621d6a
Artyom <yom@artyom.me>
2016-02-13 17:27:00 +0300

    Add tests for generic instances of data families

--- tests/DataFamilies/Types.hs
+++ tests/DataFamilies/Types.hs
@@ -2,15 +2,18 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE TypeFamilies #-}
 
 module DataFamilies.Types where
 
 import Types (ApproxEq(..))
 
+import GHC.Generics
+
 data family Nullary a
-data instance Nullary Int  = C1 | C2 | C3 deriving (Eq, Show)
-data instance Nullary Char = C4           deriving (Eq, Show)
+data instance Nullary Int  = C1 | C2 | C3 deriving (Eq, Show, Generic)
+data instance Nullary Char = C4           deriving (Eq, Show, Generic)
 
 data family SomeType a b c
 data instance SomeType c () a = Nullary
@@ -19,11 +22,11 @@ data instance SomeType c () a = Nullary
                               | Record { testOne   :: Double
                                        , testTwo   :: Maybe Bool
                                        , testThree :: Maybe a
-                                       } deriving (Eq, Show)
+                                       } deriving (Eq, Show, Generic)
 
 data family Approx a
 newtype instance Approx a = Approx { fromApprox :: a }
-    deriving (Show, ApproxEq, Num)
+    deriving (Show, ApproxEq, Num, Generic)
 
 instance (ApproxEq a) => Eq (Approx a) where
     Approx a == Approx b = a =~ b
