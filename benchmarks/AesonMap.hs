e938bf39e4c6d2230aea9ad6f949f6f78e4159e6
Oleg Grenrus <oleg.grenrus@iki.fi>
2016-05-25 10:00:29 +0300

    Add AesonMap benchmark

--- benchmarks/AesonMap.hs
+++ benchmarks/AesonMap.hs
@@ -1,10 +1,11 @@
-{-# LANGUAGE BangPatterns, OverloadedStrings, FlexibleInstances, MultiParamTypeClasses, DataKinds #-}
+{-# LANGUAGE OverloadedStrings, RankNTypes #-}
 
 import Control.DeepSeq
 import Criterion.Main
 import Data.Aeson
-import Data.Aeson.Types (Parser)
+-- import Data.Aeson.Types (Parser)
 import Data.Hashable
+import Data.Proxy (Proxy (..))
 import qualified Data.ByteString.Lazy as LBS
 import qualified Data.Text as T
 import qualified Data.Map as M
@@ -15,6 +16,10 @@ value n = HM.fromList $ map f [1..n]
   where
     f m = let t = T.pack (show m) in (t, t)
 
+-------------------------------------------------------------------------------
+-- Text 
+-------------------------------------------------------------------------------
+
 newtype T1 = T1 T.Text
   deriving (Eq, Ord)
 
@@ -22,9 +27,16 @@ instance NFData T1 where
   rnf (T1 t) = rnf t
 instance Hashable T1 where
   hashWithSalt salt (T1 t) = hashWithSalt salt t
-instance FromJSONKey T1 'JSONKeyIdentity where
-    fromJSONKey _ = T1
+instance FromJSON T1 where
+    parseJSON = withText "T1" $ pure . T1
+instance FromJSONKey T1 where
+    fromJSONKey = FromJSONKeyText T1
 
+-------------------------------------------------------------------------------
+-- Coerce
+-------------------------------------------------------------------------------
+
+-- | TODO: same as T1 for now
 newtype T2 = T2 T.Text
   deriving (Eq, Ord)
 
@@ -32,8 +44,14 @@ instance NFData T2 where
   rnf (T2 t) = rnf t
 instance Hashable T2 where
   hashWithSalt salt (T2 t) = hashWithSalt salt t
-instance FromJSONKey T2 'JSONKeyCoerce where
-    fromJSONKey _ = ()
+instance FromJSON T2 where
+    parseJSON = withText "T2" $ pure . T2
+instance FromJSONKey T2 where
+    fromJSONKey = FromJSONKeyText T2
+
+-------------------------------------------------------------------------------
+-- TextParser
+-------------------------------------------------------------------------------
 
 newtype T3 = T3 T.Text
   deriving (Eq, Ord)
@@ -42,8 +60,14 @@ instance NFData T3 where
   rnf (T3 t) = rnf t
 instance Hashable T3 where
   hashWithSalt salt (T3 t) = hashWithSalt salt t
-instance FromJSONKey T3 'JSONKeyTextParser where
-    fromJSONKey _ = return . T3
+instance FromJSON T3 where
+    parseJSON = withText "T3" $ pure . T3
+instance FromJSONKey T3 where
+    fromJSONKey = FromJSONKeyTextParser (pure . T3)
+
+-------------------------------------------------------------------------------
+-- Values
+-------------------------------------------------------------------------------
 
 encodedValue10 :: LBS.ByteString
 encodedValue10 = encode $ value 10
@@ -57,59 +81,75 @@ encodedValue1000 = encode $ value 1000
 encodedValue10000 :: LBS.ByteString
 encodedValue10000 = encode $ value 10000
 
+-------------------------------------------------------------------------------
+-- Helpers
+-------------------------------------------------------------------------------
+
+decodeHM
+    :: (FromJSONKey k, Eq k, Hashable k)
+    => Proxy k -> LBS.ByteString -> Maybe (HM.HashMap k T.Text)
+decodeHM _ = decode
+
+decodeMap
+    :: (FromJSONKey k, Ord k)
+    => Proxy k -> LBS.ByteString -> Maybe (M.Map k T.Text)
+decodeMap _ = decode
+
+proxyText :: Proxy T.Text
+proxyText = Proxy
+
+proxyT1 :: Proxy T1
+proxyT1 = Proxy
+
+proxyT2 :: Proxy T2
+proxyT2 = Proxy
+
+proxyT3 :: Proxy T3
+proxyT3 = Proxy
+
+-------------------------------------------------------------------------------
+-- Main
+-------------------------------------------------------------------------------
+
+benchDecodeHM
+    :: String
+    -> LBS.ByteString
+    -> Benchmark
+benchDecodeHM name val = 
+    bgroup name
+        [  bench "Text"     $ nf (decodeHM proxyText) val
+        ,  bench "Identity" $ nf (decodeHM proxyT1)   val
+        ,  bench "Coerce"   $ nf (decodeHM proxyT2)   val
+        ,  bench "Parser"   $ nf (decodeHM proxyT3)   val
+        ]
+
+benchDecodeMap
+    :: String
+    -> LBS.ByteString
+    -> Benchmark
+benchDecodeMap name val = 
+    bgroup name
+        [  bench "Text"     $ nf (decodeMap proxyText) val
+        ,  bench "Identity" $ nf (decodeMap proxyT1)   val
+        ,  bench "Coerce"   $ nf (decodeMap proxyT2)   val
+        ,  bench "Parser"   $ nf (decodeMap proxyT3)   val
+        ]
+
 main :: IO ()
 main = do
   defaultMain
-    [ bgroup "HashMap"
-      [ bgroup "10"
-        [  bench "Text" $ nf (decode :: LBS.ByteString -> Maybe (HM.HashMap T.Text T.Text)) encodedValue10
-        ,  bench "Identity" $ nf (decode :: LBS.ByteString -> Maybe (HM.HashMap T1 T.Text)) encodedValue10
-        ,  bench "Coerce" $ nf (decode :: LBS.ByteString -> Maybe (HM.HashMap T2 T.Text)) encodedValue10
-        ,  bench "Parser" $ nf (decode :: LBS.ByteString -> Maybe (HM.HashMap T3 T.Text)) encodedValue10
+    [ bgroup "encode"
+      [ bgroup "HashMap"
+        [ benchDecodeHM "10"    encodedValue10
+        , benchDecodeHM "100"   encodedValue100
+        , benchDecodeHM "1000"  encodedValue1000
+        , benchDecodeHM "10000" encodedValue10000
         ]
-      , bgroup "100"
-        [  bench "Text" $ nf (decode :: LBS.ByteString -> Maybe (HM.HashMap T.Text T.Text)) encodedValue100
-        ,  bench "Identity" $ nf (decode :: LBS.ByteString -> Maybe (HM.HashMap T1 T.Text)) encodedValue100
-        ,  bench "Coerce" $ nf (decode :: LBS.ByteString -> Maybe (HM.HashMap T2 T.Text)) encodedValue100
-        ,  bench "Parser" $ nf (decode :: LBS.ByteString -> Maybe (HM.HashMap T3 T.Text)) encodedValue100
-        ]
-      , bgroup "1000"
-        [  bench "Text" $ nf (decode :: LBS.ByteString -> Maybe (HM.HashMap T.Text T.Text)) encodedValue1000
-        ,  bench "Identity" $ nf (decode :: LBS.ByteString -> Maybe (HM.HashMap T1 T.Text)) encodedValue1000
-        ,  bench "Coerce" $ nf (decode :: LBS.ByteString -> Maybe (HM.HashMap T2 T.Text)) encodedValue1000
-        ,  bench "Parser" $ nf (decode :: LBS.ByteString -> Maybe (HM.HashMap T3 T.Text)) encodedValue1000
-        ]
-      , bgroup "10000"
-        [  bench "Text" $ nf (decode :: LBS.ByteString -> Maybe (HM.HashMap T.Text T.Text)) encodedValue10000
-        ,  bench "Identity" $ nf (decode :: LBS.ByteString -> Maybe (HM.HashMap T1 T.Text)) encodedValue10000
-        ,  bench "Coerce" $ nf (decode :: LBS.ByteString -> Maybe (HM.HashMap T2 T.Text)) encodedValue10000
-        ,  bench "Parser" $ nf (decode :: LBS.ByteString -> Maybe (HM.HashMap T3 T.Text)) encodedValue10000
-        ]
-      ]
-    , bgroup "Map"
-      [ bgroup "10"
-        [  bench "Text" $ nf (decode :: LBS.ByteString -> Maybe (M.Map T.Text T.Text)) encodedValue10
-        ,  bench "Identity" $ nf (decode :: LBS.ByteString -> Maybe (M.Map T1 T.Text)) encodedValue10
-        ,  bench "Coerce" $ nf (decode :: LBS.ByteString -> Maybe (M.Map T2 T.Text)) encodedValue10
-        ,  bench "Parser" $ nf (decode :: LBS.ByteString -> Maybe (M.Map T3 T.Text)) encodedValue10
-        ]
-      , bgroup "100"
-        [  bench "Text" $ nf (decode :: LBS.ByteString -> Maybe (M.Map T.Text T.Text)) encodedValue100
-        ,  bench "Identity" $ nf (decode :: LBS.ByteString -> Maybe (M.Map T1 T.Text)) encodedValue100
-        ,  bench "Coerce" $ nf (decode :: LBS.ByteString -> Maybe (M.Map T2 T.Text)) encodedValue100
-        ,  bench "Parser" $ nf (decode :: LBS.ByteString -> Maybe (M.Map T3 T.Text)) encodedValue100
-        ]
-      , bgroup "1000"
-        [  bench "Text" $ nf (decode :: LBS.ByteString -> Maybe (M.Map T.Text T.Text)) encodedValue1000
-        ,  bench "Identity" $ nf (decode :: LBS.ByteString -> Maybe (M.Map T1 T.Text)) encodedValue1000
-        ,  bench "Coerce" $ nf (decode :: LBS.ByteString -> Maybe (M.Map T2 T.Text)) encodedValue1000
-        ,  bench "Parser" $ nf (decode :: LBS.ByteString -> Maybe (M.Map T3 T.Text)) encodedValue1000
-        ]
-      , bgroup "10000"
-        [  bench "Text" $ nf (decode :: LBS.ByteString -> Maybe (M.Map T.Text T.Text)) encodedValue10000
-        ,  bench "Identity" $ nf (decode :: LBS.ByteString -> Maybe (M.Map T1 T.Text)) encodedValue10000
-        ,  bench "Coerce" $ nf (decode :: LBS.ByteString -> Maybe (M.Map T2 T.Text)) encodedValue10000
-        ,  bench "Parser" $ nf (decode :: LBS.ByteString -> Maybe (M.Map T3 T.Text)) encodedValue10000
+      , bgroup "Map"
+        [ benchDecodeMap "10"    encodedValue10
+        , benchDecodeMap "100"   encodedValue100
+        , benchDecodeMap "1000"  encodedValue1000
+        , benchDecodeMap "10000" encodedValue10000
         ]
       ]
     ]
