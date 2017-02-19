35ae44dbec513b45d4ec94d95588c56d35b7f88a
Adam Bergmark <adam@bergmark.nl>
2017-02-19 14:54:08 +0100

    stack & travis updates

--- travis/script.sh
+++ travis/script.sh
@@ -4,7 +4,7 @@ set -ex
 
 case $BUILD in
   stack)
-    stack build --test --haddock
+    stack build --test --haddock --no-terminal
     ;;
   cabal)
     if [ -f configure.ac ]; then autoreconf -i; fi
