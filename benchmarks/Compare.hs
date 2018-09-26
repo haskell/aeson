d1a6b73cc5bde54f1fffe10ed396b76af62aeee4
Oleg Grenrus <oleg.grenrus@iki.fi>
2018-09-26 12:10:51 +0300

    Make benchmarks compile with ghc-8.4+

--- benchmarks/Compare.hs
+++ benchmarks/Compare.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
@@ -6,15 +7,18 @@ module Main (main) where
 import Prelude.Compat
 
 import Compare.BufferBuilder ()
-import Compare.JsonBuilder ()
 import Criterion.Main
 import Data.BufferBuilder.Json
-import Data.Json.Builder
 import Twitter
 import Twitter.Manual ()
 import Typed.Common
 import qualified Data.Aeson as Aeson
 import qualified Compare.JsonBench as JsonBench
+
+#ifdef MIN_VERSION_json_builder
+import Data.Json.Builder
+import Compare.JsonBuilder ()
+#endif
 
 main :: IO ()
 main =
@@ -23,7 +27,9 @@ main =
      bgroup "twitter" [
          bench "aeson" $ nf Aeson.encode twtr
        , bench "buffer-builder" $ nf encodeJson twtr
+#ifdef MIN_VERSION_json_builder
        , bench "json-builder" $ nf toJsonLBS twtr
+#endif
        ]
    , JsonBench.benchmarks
    ]
