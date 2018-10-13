38272ecca27c9487d403c8340a29e738100d61c6
Oleg Grenrus <oleg.grenrus@iki.fi>
2018-10-13 21:45:59 +0300

    Use algorithm from base to do ByteString -> Integer

--- benchmarks/Issue673.hs
+++ benchmarks/Issue673.hs
@@ -23,6 +23,7 @@ import Data.Aeson.Parser (scientific)
 import qualified Data.Attoparsec.ByteString.Lazy as AttoL
 import qualified Data.Attoparsec.ByteString.Char8 as Atto8
 import qualified Data.Aeson as A
+import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as LBS
 import qualified Data.ByteString.Lazy.Char8 as LBS8
 
@@ -35,9 +36,8 @@ decodeString = A.decode
 decodeScientific :: LBS.ByteString -> Maybe Scientific
 decodeScientific = A.decode
 
-decodeRaw :: LBS.ByteString -> Integer
-decodeRaw = LBS.foldl' step 0 where
-  step a b = a * 10 + fromIntegral (b - 48) -- 48 = '0'
+decodeViaRead :: LBS.ByteString -> Integer
+decodeViaRead = read . LBS8.unpack
 
 decodeAtto :: LBS.ByteString -> Maybe Scientific
 decodeAtto
@@ -102,9 +102,63 @@ main =  defaultMain
   where
     benchPair name input = bgroup name
         [ bench "Int"        $ whnf decodeInt input
-        , bench "RawFold"    $ whnf decodeRaw input
+        , bench "Simple"     $ whnf bsToIntegerSimple (LBS.toStrict input)
+        , bench "Optim"      $ whnf bsToInteger (LBS.toStrict input)
+        , bench "Read"       $ whnf decodeViaRead input
         , bench "Scientific" $ whnf decodeScientific input
         , bench "parserA"    $ whnf decodeAtto  input
         , bench "parserS"    $ whnf decodeAtto8  input
         , bench "String"     $ whnf decodeString $ "\"" <> input <> "\""
         ]
+
+-------------------------------------------------------------------------------
+-- better fromInteger
+-------------------------------------------------------------------------------
+
+bsToInteger :: BS.ByteString -> Integer
+bsToInteger bs
+    | l > 40    = valInteger 10 l [ fromIntegral (w - 48) | w <- BS.unpack bs ]
+    | otherwise = bsToIntegerSimple bs
+  where
+    l = BS.length bs
+
+bsToIntegerSimple :: BS.ByteString -> Integer
+bsToIntegerSimple = BS.foldl' step 0 where
+  step a b = a * 10 + fromIntegral (b - 48) -- 48 = '0'
+
+-- A sub-quadratic algorithm for Integer. Pairs of adjacent radix b
+-- digits are combined into a single radix b^2 digit. This process is
+-- repeated until we are left with a single digit. This algorithm
+-- performs well only on large inputs, so we use the simple algorithm
+-- for smaller inputs.
+valInteger :: Integer -> Int -> [Integer] -> Integer
+valInteger = go
+  where
+    go :: Integer -> Int -> [Integer] -> Integer
+    go _ _ []  = 0
+    go _ _ [d] = d
+    go b l ds
+        | l > 40 = b' `seq` go b' l' (combine b ds')
+        | otherwise = valSimple b ds
+      where
+        -- ensure that we have an even number of digits
+        -- before we call combine:
+        ds' = if even l then ds else 0 : ds
+        b' = b * b
+        l' = (l + 1) `quot` 2
+
+    combine b (d1 : d2 : ds) = d `seq` (d : combine b ds)
+      where
+        d = d1 * b + d2
+    combine _ []  = []
+    combine _ [_] = errorWithoutStackTrace "this should not happen"
+
+-- The following algorithm is only linear for types whose Num operations
+-- are in constant time.
+valSimple :: Integer -> [Integer] -> Integer
+valSimple base = go 0
+  where
+    go r [] = r
+    go r (d : ds) = r' `seq` go r' ds
+      where
+        r' = r * base + fromIntegral d
