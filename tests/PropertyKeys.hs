3f553c488c45d617c3e37cc0ada8668847b99e8f
Kevin Quick <kquick@galois.com>
2019-01-01 19:43:59 -0800

    Split Properties tests into multiple files.
    The original property testing used a single file to specify all
    property tests, which caused compilation issues and intermittent
    Travis CI build failures.  This commit breaks the Properties file into
    multiple files to make it faster to compile.
    
        | Properties  |   GHC | QuickCheck | Clean build/test      | properties build/test |
        |-------------+-------+------------+-----------------------+-----------------------|
        | single file | 8.4.4 |     2.11.3 | 3m48s / 5m56s / 1m33s | 1m42s / 1m58s / 0m31s |
        |             |       |   2.12.6.1 | 3m27s / 5m42s / 0m58s | 1m45s / 2m2s / 0m33s  |
        |-------------+-------+------------+-----------------------+-----------------------|
        | multi-file  | 8.4.4 |     2.11.3 | 2m19s / 4m26s / 0m37s | 0m35s / 0m44s / 0m9s  |
        |             |       |   2.12.6.1 | 2m20s / 4m26s / 0m37s | 0m36s / 0m45s / 0m10s |
    
    (times are in real, user, and sys time)

---
+++ tests/PropertyKeys.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+
+module PropertyKeys ( keysTests ) where
+
+import Prelude.Compat
+
+import Data.Time (Day, LocalTime, TimeOfDay, UTCTime)
+import Data.Version (Version)
+import Instances ()
+import Numeric.Natural (Natural)
+import Test.Tasty (TestTree, testGroup)
+import Test.Tasty.QuickCheck (testProperty)
+import Types
+import qualified Data.Text as T
+import qualified Data.Text.Lazy as LT
+import qualified Data.UUID.Types as UUID
+import PropUtils
+
+
+keysTests :: TestTree
+keysTests =
+  testGroup "roundTrip Key"
+    [ testProperty "Bool" $ roundTripKey True
+    , testProperty "Text" $ roundTripKey (undefined :: T.Text)
+    , testProperty "String" $ roundTripKey (undefined :: String)
+    , testProperty "Int" $ roundTripKey (undefined :: Int)
+    , testProperty "[Text]" $ roundTripKey (undefined :: LogScaled [T.Text])
+    , testProperty "(Int,Char)" $ roundTripKey (undefined :: (Int,Char))
+    , testProperty "Integer" $ roundTripKey (undefined :: Integer)
+    , testProperty "Natural" $ roundTripKey (undefined :: Natural)
+    , testProperty "Float" $ roundTripKey (undefined :: Float)
+    , testProperty "Double" $ roundTripKey (undefined :: Double)
+#if MIN_VERSION_base(4,7,0)
+    , testProperty "Day" $ roundTripKey (undefined :: Day)
+    , testProperty "LocalTime" $ roundTripKey (undefined :: LocalTime)
+    , testProperty "TimeOfDay" $ roundTripKey (undefined :: TimeOfDay)
+    , testProperty "UTCTime" $ roundTripKey (undefined :: UTCTime)
+#endif
+    , testProperty "Version" $ roundTripKey (undefined :: Version)
+    , testProperty "Lazy Text" $ roundTripKey (undefined :: LT.Text)
+    , testProperty "UUID" $ roundTripKey UUID.nil
+    ]
---
+++ tests/PropertyRoundTrip.hs
@@ -0,0 +1,67 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
+module PropertyRoundTrip ( roundTripTests ) where
+
+import Prelude.Compat
+
+import Control.Applicative (Const)
+import Data.Aeson.Types
+import Data.DList (DList)
+import Data.List.NonEmpty (NonEmpty)
+import Data.Proxy (Proxy)
+import Data.Ratio (Ratio)
+import Data.Sequence (Seq)
+import Data.Tagged (Tagged)
+import Data.Time (Day, DiffTime, LocalTime, NominalDiffTime, TimeOfDay, UTCTime, ZonedTime)
+import Data.Version (Version)
+import Instances ()
+import Numeric.Natural (Natural)
+import Test.Tasty (TestTree, testGroup)
+import Test.Tasty.QuickCheck (testProperty)
+import Types
+import qualified Data.Text as T
+import qualified Data.Text.Lazy as LT
+import qualified Data.UUID.Types as UUID
+import PropUtils
+import PropertyRTFunctors
+
+
+roundTripTests :: TestTree
+roundTripTests =
+  testGroup "roundTrip" [
+      testProperty "Bool" $ roundTripEq True
+    , testProperty "Double" $ roundTripEq (1 :: Approx Double)
+    , testProperty "Int" $ roundTripEq (1 :: Int)
+    , testProperty "NonEmpty Char" $ roundTripEq (undefined :: NonEmpty Char)
+    , testProperty "Integer" $ roundTripEq (1 :: Integer)
+    , testProperty "String" $ roundTripEq ("" :: String)
+    , testProperty "Text" $ roundTripEq T.empty
+    , testProperty "Lazy Text" $ roundTripEq LT.empty
+    , testProperty "Foo" $ roundTripEq (undefined :: Foo)
+    , testProperty "Day" $ roundTripEq (undefined :: Day)
+    , testProperty "BCE Day" $ roundTripEq (undefined :: BCEDay)
+    , testProperty "DotNetTime" $ roundTripEq (undefined :: Approx DotNetTime)
+    , testProperty "LocalTime" $ roundTripEq (undefined :: LocalTime)
+    , testProperty "TimeOfDay" $ roundTripEq (undefined :: TimeOfDay)
+    , testProperty "UTCTime" $ roundTripEq (undefined :: UTCTime)
+    , testProperty "ZonedTime" $ roundTripEq (undefined :: ZonedTime)
+    , testProperty "NominalDiffTime" $ roundTripEq (undefined :: NominalDiffTime)
+    , testProperty "DiffTime" $ roundTripEq (undefined :: DiffTime)
+    , testProperty "Version" $ roundTripEq (undefined :: Version)
+    , testProperty "Natural" $ roundTripEq (undefined :: Natural)
+    , testProperty "Proxy" $ roundTripEq (undefined :: Proxy Int)
+    , testProperty "Tagged" $ roundTripEq (undefined :: Tagged Int Char)
+    , testProperty "Const" $ roundTripEq (undefined :: Const Int Char)
+    , testProperty "DList" $ roundTripEq (undefined :: DList Int)
+    , testProperty "Seq" $ roundTripEq (undefined :: Seq Int)
+    , testProperty "Rational" $ roundTripEq (undefined :: Rational)
+    , testProperty "Ratio Int" $ roundTripEq (undefined :: Ratio Int)
+    , testProperty "UUID" $ roundTripEq UUID.nil
+    , roundTripFunctorsTests
+    , testGroup "ghcGenerics" [
+        testProperty "OneConstructor" $ roundTripEq OneConstructor
+      , testProperty "Product2" $ roundTripEq (undefined :: Product2 Int Bool)
+      , testProperty "Product6" $ roundTripEq (undefined :: P6)
+      , testProperty "Sum4" $ roundTripEq (undefined :: S4)
+      ]
+    ]
