ed4f5968868b67aedb070421690412ace715c818
Oleg Grenrus <oleg.grenrus@iki.fi>
2021-12-06 19:17:43 +0200

    unsafer unsafeDecodeASCII

--- src/Data/Aeson/Internal/Text.hs
+++ src/Data/Aeson/Internal/Text.hs
@@ -1,12 +1,25 @@
-{-# LANGUAGE CPP #-}
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE CPP          #-}
 module Data.Aeson.Internal.Text (
     unsafeDecodeASCII,
 ) where
 
-import           Data.ByteString    (ByteString)
-import           Data.Text          (Text)
+import           Data.ByteString                (ByteString)
+import           Data.Text                      (Text, empty)
 
-import qualified Data.Text.Encoding as TE
+
+#if MIN_VERSION_text(2,0,0)
+import           Data.Text.Array                (Array (..))
+import           Data.Text.Internal             (Text (..))
+
+import qualified Data.ByteString.Short.Internal as SBS
+
+import           Data.Aeson.Internal.ByteString
+
+#else
+import qualified Data.Text.Encoding             as TE
+
+#endif
 
 -- | The input is assumed to contain only 7bit ASCII characters (i.e. @< 0x80@).
 --   We use TE.decodeLatin1 here because TE.decodeASCII is currently (text-1.2.4.0)
@@ -14,7 +27,9 @@ import qualified Data.Text.Encoding as TE
 unsafeDecodeASCII :: ByteString -> Text
 
 #if MIN_VERSION_text(2,0,0)
-unsafeDecodeASCII = TE.decodeASCII
+unsafeDecodeASCII bs = withBS bs $ \_fp len -> if len == 0 then empty else
+  let !(SBS.SBS arr) = SBS.toShort bs in Text (ByteArray arr) 0 len
+
 #else
 unsafeDecodeASCII = TE.decodeLatin1
 #endif
